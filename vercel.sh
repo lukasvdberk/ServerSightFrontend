15a31f4491b6c3b252cb5176d6255884ff8f8d7b
lukasberk <lukasvdberk@gmail.com>
2021-09-23 20:46:46 +0200

    used build instead of export

--- vercel.sh
+++ vercel.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "export const BASE_MEDIA_URL = 'https://serversight.lukas.sh/assets/'" >> src/config.ts
 echo "export const BASE_API_URL = 'https://serversight.lukas.sh/api/'" >> src/config.ts
-npm run export
+npm run build
