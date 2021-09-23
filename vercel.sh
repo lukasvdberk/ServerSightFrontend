bbd90bb3ec1596e07f824df5a3f64717771dfc38
lukasberk <lukasvdberk@gmail.com>
2021-09-23 21:09:53 +0200

    switched to static output

--- vercel.sh
+++ vercel.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "export const BASE_MEDIA_URL = 'https://serversight.lukas.sh/assets/'" >> src/config.ts
 echo "export const BASE_API_URL = 'https://serversight.lukas.sh/api/'" >> src/config.ts
-npx sapper export --basepath
+sapper export --legacy --static
